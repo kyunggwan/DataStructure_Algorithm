7cb2d7334ea617b0a36c86ae95871f273ac056fd
4562sky <4562sky@naver.com>
2022-10-31 12:09:59 +0900

    미로찾기

--- Maze/MazingProblem.java
+++ Maze/MazingProblem.java
@@ -1,7 +1,6 @@
-package Maze;
+package MazeProblem;
 
-import Maze.Item.MazeStack;
-
+//좌표를 저장하는 Offsets
 class Offsets {
 	public int a;
 	public int b;
@@ -19,136 +18,17 @@ class Offsets {
 }
 
 public class MazingProblem {
-	// 테두리 설정.
-//	for (int i = 0; i < maze.length; i++) {
-//		for (int j = 0; j < maze[i].length; j++) {
-//			System.out.print(maze[i][j] + " ");
-//		}
-//		System.out.println("");
-//	}
-//	
 
-	// Item 스택을 만듦.
-
-	//
-	static Offsets moves[] = new Offsets[8];
-
-	// 방향을 설정, 북쪽을 0으로 시계방향으로 방향을 나타냄
-//	int moves[][] = {
-//			{ 0, 1 }, // N
-//			{ 1, 1 }, // NE
-//			{ 1, 0 }, // E
-//			{ 1, -1 }, // SE
-//			{ 0, -1 }, // S
-//			{ -1, -1 }, // SW
-//			{ -1, 0 }, // W
-//			{ -1, 1 } // NW
-//	};
-
-//path 메소드
 	static void path(int[][] maze, int[][] mark, int x, int y) {
-		MazeStack st = new MazeStack(100);
-		// 시작점 1,1
-		mark[1][1] = 1;
-		int ix, iy, dir;
-		// 시작 점에서 나감
-		Item temp = new Item(0, 0, 0);
-		temp.x = 1;
-		temp.y = 1;
-		temp.dir = 2;
-		st.push(temp);
-		System.out.println("초기:: " + temp.toString());
+		// 스택을 불러옴
+		Stack st = new Stack(100);
+		// moves[]라는 8개짜리 배열 하나 만듦
+		Offsets moves[] = new Offsets[8];
+		// moves배열 안에 Offsets객체를 만듦. moves는 위에서 설정한 대로 8개까지 가능
+		for (int i = 0; i < 8; i++)
+			moves[i] = new Offsets(0, 0);
 
-		while (!st.isEmpty()) // stack not empty
-		{
-			temp = st.pop();
-
-			int i = temp.x;
-			int k = temp.y;
-			int d = temp.dir;
-
-			while (d < 8) // moves forward
-			{
-				System.out.println(i + " " + k + " " + d); // output path
-
-				int g = i + moves[d].a;
-
-				int h = k + moves[d].b;
-
-				if ((g == ix) && (h == iy)) { // reached exit
-					// output path
-					System.out.println("the term near the exit: " + i + " " + k);
-					System.out.println("exit: " + ix + " " + iy);
-					return;
-				}
-				if ((maze[g][h] == 0) && (mark[g][h] == 0)) { // new position
-
-				} else
-					d++; // try next direction
-			}
-		}
-		System.out.println("no path in maze ");
-	}
-
-	public static void main(String[] args) {
-		int[][] maze = new int[100][100];
-		int[][] mark = new int[100][100];
-
-//void path(int m, int p)
-////Output a path (if any) in the maze; rows is m, cols is p;
-//{
-//	//start at (1,1)
-//	mark[1][1] = 1;
-//	Stack<items> stack(m * p);
-//	items temp;
-//	temp.x = 1; temp.y = 1; temp.dir = E;
-//	stack.Push(temp);
-//
-//	while (!stack.IsEmpty()) // stack not empty
-//	{
-//		temp = stack.Pop(); // unstack
-//		int i = temp.x; int j = temp.y; int d = temp.dir;
-//		while (d < 8) // moves forward
-//		{
-//			//outFile << i << " " << j << " " << d << endl;
-//			int g = i + moves[d].a;
-//			int h = j + moves[d].b;
-//			if ((g == m) && (h == p)) { // reached exit
-//										// output path
-//				cout << stack;
-//
-//				cout << "the term near the exit: " << i << " " << j << endl;
-//				cout << "exit: " << m << " " << p << endl;
-//				return;
-//			}
-//			if ((!maze[g][h]) && (!mark[g][h])) { // new position
-//				mark[g][h] = 1;
-//				//push the old temp to the stack, but the direction changes.
-//				//Because the neighbor in the direction of d has been checked.
-//				temp.x = i;  temp.y = j; temp.dir = d + 1;
-//				stack.Push(temp); // stack it
-//				i = g; j = h; d = N; // moves to (g,h)
-//			}
-//			else d++; // try next direction
-//		}
-//	}
-//	cout << "no path in maze " << endl;
-//}
-///*
-//maze[][], mark[][]를 class의 private data member로 선언
-//main()에서는 class의 public function의 호출로 미로 찾기
-//*/
-//void main() {
-
-		int input[][] = { // 12 x 15
-				{ 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1 }, { 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1 },
-				{ 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1 }, { 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0 },
-				{ 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1 }, { 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1 },
-				{ 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1 }, { 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1 },
-				{ 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1 }, { 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0 },
-				{ 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0 }, { 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0 }, };
-
-		moves[0].a = -1;
+		moves[0].a = -1; // 서쪽
 		moves[0].b = 0;
 		moves[1].a = -1;
 		moves[1].b = 1;
@@ -165,7 +45,78 @@ public class MazingProblem {
 		moves[7].a = -1;
 		moves[7].b = -1;
 
-		// i가 0~14이고, j가 0~17일때, maze[i][j]=1안간다.
+		// 시작점 1,1
+		mark[1][1] = 1;
+
+		// Item은 임시좌표 저장소, temp라고 명명
+		Item temp = new Item(0, 0, 0);
+		// 초기값으로 (1,1,1)로 시작
+		temp.setX(1);
+		temp.setY(1);
+		temp.setDir(2);
+		st.push(temp);
+		System.out.println("초기:: " + temp.toString());
+
+		while (!st.isEmpty()) // stack not empty, 스택이 공백이 될때까지 반복
+		{
+			// temp = 지나온 경로를 pop해라
+			temp = st.pop();
+			// ix, iy는 좌표 저장 주소
+			int ix = temp.getX();
+			int iy = temp.getY();
+			int dir = temp.getDir();
+
+			while (dir < 8) // moves forward, d는 방향, 8방위를 조사
+			{
+//				System.out.println(ix + " " + iy + " " + dir); // output path
+				// 가려고 하는 방향으로 x+, y+
+				int nextX = ix + moves[dir].a; // x - 1
+				int nextY = iy + moves[dir].b; // y
+
+				if ((nextX == 15) && (nextY == 12)) { // reached exit, 출구 발견시에
+					// output path, 마지막 경로 지점을 푸시
+					st.push(new Item(ix, iy, dir));
+					// 마지막 지점까지 지정
+					st.push(new Item(nextX, nextY, dir));
+
+//					System.out.println("the term near the exit: " + ix + " " + iy);
+					System.out.println("exit: " + 12 + " " + 15);
+					return;
+				}
+				if ((maze[nextX][nextY] == 0) && (mark[nextX][nextY] == 0)) { // new position 시도해보지 않은 위치, 이동가능 위치
+					// 다음 위치 발견하면 기존 위치를 mark에 1로 저장
+					mark[ix][iy] = 1;
+					// 매번 객체를 만들어서 저장
+					Item tmp = new Item(ix, iy, dir + 1);
+					// 그 좌표를 스택에 push
+					st.push(tmp);
+					// 다음 좌표로 전환
+					ix = nextX;
+					iy = nextY;
+					// dir을 초기화 해줘야 방향을 처음부터 다시 검사함.
+					dir = 0;
+
+				} else
+					dir++; // try next direction
+			}
+		}
+		System.out.println("no path in maze ");
+	}
+
+	public static void main(String[] args) {
+		// maze, mark를 만듦.
+		int[][] maze = new int[14][17];
+		int[][] mark = new int[14][17];
+
+		int input[][] = { // 12 x 15
+				{ 0, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1 }, { 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 0, 0, 1, 1, 1 },
+				{ 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1 }, { 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 0, 1, 1, 0, 0 },
+				{ 1, 1, 0, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1 }, { 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1 },
+				{ 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 0, 0, 1, 0, 1 }, { 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1 },
+				{ 0, 0, 1, 1, 0, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1 }, { 1, 1, 0, 0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0 },
+				{ 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0 }, { 0, 1, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 0 }, };
+
+		// 미로의 테두리에 1의 벽을 침.
 		for (int i = 0; i < 14; i++) {
 			for (int j = 0; j < 17; j++) {
 				if ((i == 0) || (j == 0) || (i == 13) || (j == 16))
@@ -178,6 +129,7 @@ public class MazingProblem {
 
 			}
 		}
+		// 미로 출력
 		System.out.println("maze[12,15]::");
 		for (int i = 0; i <= 13; i++) {
 			for (int j = 0; j <= 16; j++) {
@@ -186,14 +138,7 @@ public class MazingProblem {
 			}
 			System.out.println();
 		}
-		System.out.println("mark::");
-		for (int i = 0; i <= 13; i++) {
-			for (int j = 0; j <= 16; j++) {
-				System.out.print(mark[i][j] + " ");
-
-			}
-			System.out.println();
-		}
+		// 지나가는 길 출력
 		path(maze, mark, 12, 15);
 		System.out.println("mark::");
 		for (int i = 1; i <= 12; i++) {
